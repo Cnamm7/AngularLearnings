6ae0c2eabfa239adcc16e97a6e7f71f036e8eef3
Cnamm7 <cna.mm7@gmail.com>
2022-06-04 00:21:38 -0400

    Create new project

---
+++ ThirdProject/banks/src/app/banks/banks.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Bank } from '../bank';
+import { Banks } from '../banks';
+
+@Component({
+  selector: 'app-banks',
+  templateUrl: './banks.component.html',
+  styleUrls: ['./banks.component.css']
+})
+export class BanksComponent implements OnInit {
+  banks = Banks;
+  selectedBank: Bank;
+
+  constructor() { this.selectedBank = {id: 0, name: ""};}
+
+  onSelect(bank: Bank): void {
+    this.selectedBank = bank;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
